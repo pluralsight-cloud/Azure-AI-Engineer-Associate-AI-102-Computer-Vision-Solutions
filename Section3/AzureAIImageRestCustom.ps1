9593ee86bdc0a907ed1f4a41a30b947e20083d65
John Savill <john@savilltech.com>
2024-04-18 07:55:04 -0700

    Fixed script with preview API version.

--- Section3/AzureAIImageRestCustom.ps1
+++ Section3/AzureAIImageRestCustom.ps1
@@ -1,8 +1,8 @@
 $endpoint = $env:AI_SVC_ENDPOINT
-$read_image_url = "https://raw.githubusercontent.com/johnthebrit/RandomStuff/master/Misc/dogsongrass.jpg"
+$read_image_url = "https://raw.githubusercontent.com/pluralsight-cloud/Azure-AI-Engineer-Associate-AI-102-Computer-Vision-Solutions/master/Section3/TrainingData/Unknown%201.jpg"
 
 #We shall call the Read, Objects and Tags features
-$ai_url = $endpoint + "computervision/imageanalysis:analyze?api-version=2024-02-01&model-name=orangesandapples"
+$ai_url = $endpoint + "computervision/imageanalysis:analyze?api-version=2023-02-01-preview&model-name=orangesandapples"
 
 $headers = @{
     'Content-Type'='application/json'
@@ -15,8 +15,4 @@ $body = @{
 $resp = Invoke-RestMethod -Uri $ai_url -Method Post -Headers $headers -Body $body
 
 #Objects
-$resp.objectsResult.values
-#Tags
-$resp.tagsResult.values
-#Read
-$resp.readResult.blocks.lines
+$resp.customModelResult.objectsResult.values
